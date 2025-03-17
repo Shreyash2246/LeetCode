3a72109d306b25b114662a89dba4d28defe2eb74
Shreyash2246 <apatil2246b@gmail.com>
2025-03-18 02:44:20 +0530

    Updated files

---
+++ gcdOfStrings.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+class Solution{
+    public static String _gcdOfStrings(String str1, String str2){
+        if(!(str1 + str2).equals(str1+str2)){
+            return "";
+        }
+    
+        int gcdlen = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdlen);
+    }
+
+    private static int gcd(int a, int b){
+        return b==0 ? a:gcd(b, a%b);
+    }
+}
+    
+public class gcdOfStrings {
+    public static void main(String[] args) {
+        String str1 = "ABCABC", str2 = "ABC";
+    
+        System.out.println(Solution._gcdOfStrings(str1, str2));
+    }
+}
