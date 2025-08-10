cb5b3cc337b25d0635660fe1aaf9413aab9d715d
Shreyash Sanjay Patil <159467930+Shreyash2246@users.noreply.github.com>
2025-08-10 22:00:29 +0530

    Time: 0 ms (100%), Space: 56.2 MB (71.21%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -1,18 +1,21 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int n = arr.length;
-        int s = 0, e = n-1;
+        int s = 0, e = arr.length-1;
 
         while(s < e){
             int mid = s + (e-s) / 2;
 
             if(arr[mid] > arr[mid+1]){
+                // means we are in decreasing part of array.
                 e = mid;
             }else{
-                s = mid+1;
+                // we are in increasing part of array.
+                // here we do mid + 1 because if we do mid to both conditions.
+                // our loop will not get volated and it's time will get axceed. 
+                s = mid + 1;
             }
         }
 
-        return e;
+        return s;
     }
 }
