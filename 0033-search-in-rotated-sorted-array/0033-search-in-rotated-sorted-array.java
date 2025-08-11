1fec3ff9576c7bfdf4f42e43bb91b8b81b487525
Shreyash Sanjay Patil <159467930+Shreyash2246@users.noreply.github.com>
2025-08-11 22:29:29 +0530

    Time: 0 ms (100%), Space: 42.3 MB (29.07%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,15 +1,58 @@
 class Solution {
     public int search(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int start = 0, end = nums.length-1;
 
-        for(int i=0; i<nums.length; i++){
-            map.put(nums[i], i);
+        int pivot = searchPivot(nums, start, end);
+
+        if(pivot == -1){
+            return binarySearch(nums, target, 0, nums.length-1);
         }
 
-        if(map.containsKey(target)){
-            return map.get(target);
-        }else{
-            return -1;
+        if(nums[pivot] == target){
+            return pivot;
         }
+
+        if(target >= nums[0]){
+            return binarySearch(nums, target, 0, pivot-1);
+        }
+
+        return binarySearch(nums, target, pivot+1, nums.length-1);
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end){
+        
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] == target){
+                return mid;
+            }else if(target < arr[mid]){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    static int searchPivot(int arr[], int start, int end){
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            // there are four cases
+            if(mid < end && arr[mid] > arr[mid+1]){
+                return mid;
+            }else if(mid > start && arr[mid] < arr[mid-1]){
+                return mid-1;
+            }else if(arr[mid] <= arr[start]){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+
+        return -1;
     }
 }
