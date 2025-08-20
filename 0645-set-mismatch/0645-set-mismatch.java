ff397f5ea5e57e42abce43b032faec158d2e3dc0
Shreyash Sanjay Patil <159467930+Shreyash2246@users.noreply.github.com>
2025-08-20 19:17:57 +0530

    Time: 3 ms (78.19%), Space: 45.8 MB (45.39%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        // it is solved by simple cyclic sort and finding dup. by for loop
+        int n = nums.length;
+        int[] arr = new int[2];
+        
+        int i = 0;
+        while(i < n){
+            int correctIdx = nums[i]-1;
+
+            if(nums[i] != nums[correctIdx]){
+                swap(nums, i, correctIdx);
+            }else{
+                i++;
+            }
+        }
+
+        for(i = 0; i < n; i++){
+
+            if(nums[i] != i+1){
+                arr[0] = nums[i];
+                arr[1] = i+1;
+                return arr;
+            }
+        }
+
+        return arr;
+    }
+
+    static void swap(int[] arr, int s, int e){
+        int temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
+    }
+}
